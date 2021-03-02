195015c1b21507a46f87575e78a5404870ff9323
Prashant Singh <prashantsingh2408@gmail.com>
2021-03-02 17:52:42 +0530

    L5-sol

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            // solution
-            sizes: ['S', 'M', 'L', 'XL'],
-            // solution
-            variants: [
-              { id: 2234, color: 'green' },
-              { id: 2235, color: 'blue' },
-            ]
-        }
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      // solution
+      sizes: ['S', 'M', 'L', 'XL'],
+      // solution
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' },
+      ],
+      //3d array
+      array: [
+        {
+          id: 2234,
+          color: {
+            version1: 'red',
+            version2: 'black'
+          }
+        },
+      ],
+
+
     }
+  }
 })
